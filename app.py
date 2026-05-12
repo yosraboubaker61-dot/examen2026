b87335093d8ae00077120ade457e2abed1fb9794
yosra <yosraboubaker61@gmail.com>
2026-05-12 11:31:16 +0100

    mise a jpurs

--- app.py
+++ app.py
@@ -95,5 +95,5 @@ def index():
     return send_from_directory('templates', 'index.html')
 
 
-if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000, debug=False, threaded=False)
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=5000)
